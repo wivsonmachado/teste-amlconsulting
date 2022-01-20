4af5515b4e7e1b8bc76c746e6878f6b83323e0c7
Wivson Machado <wivsonfrota@gmail.com>
2022-01-20 16:33:01 -0300

    Modulo para criar tabela no banco

---
+++ createTable.js
@@ -0,0 +1,41 @@
+const client = require('./dataBaseConnection.js')
+
+
+const connDB = async () => {
+  try {
+    
+    const query = `CREATE TABLE IF NOT EXISTS cpgf (
+      cod_org_sup INT,
+      nome_org_sup VARCHAR (200),
+      cog_org INT,
+      nome_org VARCHAR (200),
+      cod_uni_ges INT,
+      nome_uni_ges VARCHAR (200),
+      ano_extrato INT,
+      mes_extrato INT,
+      cpf_portador VARCHAR (14) NULL,
+      nome_portador VARCHAR (200),
+      doc_favorecido VARCHAR(14),
+      nome_favorecido VARCHAR (200),
+      transacao VARCHAR (50),
+      data_transacao DATE NULL,
+      valor_transacao DECIMAL(15, 2)
+    );`
+
+    await client.connect()
+    await client.query(query)
+    return true
+  } catch (error) {
+    console.error(error)
+    return false
+  } finally {
+    client.end()
+  }
+}
+
+connDB().then(res => {
+  if (res) {
+    console.log(`Table created`)
+  }
+})
+
